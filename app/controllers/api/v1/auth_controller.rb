edcb782542c94c871d2d80982ac3123c58f34fd6
Colton <kaiserc2@seattleu.edu>
2020-10-16 12:36:44 -0700

    Add create functionality to auth controller.

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::AuthController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    def create
+        #Find the user based on the passed in username
+        @user = User.find_by(username: user_login_params[:username])
+
+        #If user is not nil,
+        #authenticate the password with BCrypt, create a session and issue a token
+        if @user && @user.authenticate(user_login_params[:password])
+            token = issue_token(@user)
+            render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+        else
+            render json: { message: 'Invalid username or password' }, status: :unauthorized
+        end
+    end
+
+    private
+
+    def user_login_params
+        params.require(:user).permit(:username, :password)
+    end
 end
