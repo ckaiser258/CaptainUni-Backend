551b7d86758309a3f484133b5f8a2de0cd8a183c
Colton <kaiserc2@seattleu.edu>
2020-10-16 11:23:41 -0700

    Add bcrypt has_secure_password and validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    # has_many :athletes
+
+    validates :first_name, :last_name, :username, :password, presence: { message: "must be given" }
+    validates :username, uniqueness: { message: "is taken" }
 end
