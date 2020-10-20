bc6032ad5ca64149371c7f345cc4a67f79d03b93
Colton <kaiserc2@seattleu.edu>
2020-10-19 19:41:34 -0700

    Add athletes association to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
 
-    # has_many :athletes
+    has_many :athletes
 
     validates :first_name, :last_name, :username, :password, presence: { message: "must be given" }
     validates :username, uniqueness: { message: "is taken" }
